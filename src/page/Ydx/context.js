466f17b81e35843b66a0147f966a6c6500200d7e
yangdongxu <yangdongxu@superred.com.cn>
2026-01-08 19:17:03 +0800

    ref和use

--- src/page/Ydx/context.js
+++ src/page/Ydx/context.js
@@ -1,15 +1,55 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useEffect, useRef, useState } from "react";
 
 const ThemeContext = createContext(null);
 
 export default function MyApp() {
 	return (
 		<ThemeContext.Provider value="dark">
-			<Form />
+			<>
+				<Form />
+				<Input></Input>
+			</>
 		</ThemeContext.Provider>
 	);
 }
+function Input() {
+	const inputRef = useRef(); // const inputRef=useRef(null);
+	const [count, setCount] = useState(0);
+	let ref = useRef(0);
+	/* 
+    场景 3：优化引用类型（避免重复创建对象/数组）
+    避免每次渲染都创建新的对象（{}），减少子组件不必要的渲染。
+    import { useMemo } from 'react';
 
+        function MyComponent({ style }) {
+        const defaultStyle = useMemo(() => ({ color: 'red', fontSize: 16 }), []);
+        
+        const mergedStyle = useMemo(() => ({
+            ...defaultStyle,
+            ...style,
+        }), [style, defaultStyle]);
+
+        return <div style={mergedStyle}>Hello</div>;
+        }
+    */
+	useEffect(() => {
+		// console.log(ref.current, inputRef, "==ref.current");
+		ref.current = count;
+		// ref.current++;
+		// inputRef.current.focus();
+	});
+	function handleClick() {
+		setCount(count + 1);
+		// ref.current = ref.current + 1;
+	}
+	return (
+		<>
+			<input type="text" ref={inputRef}></input>
+			<div>{ref.current}</div>
+			<button onClick={handleClick}>{count}</button>
+		</>
+	);
+}
 function Form() {
 	return (
 		<Panel title="Welcome">
